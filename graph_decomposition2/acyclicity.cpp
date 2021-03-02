f9f573bf849db0a3c527f643ed2a0cde195b3dd1
Aditya88Gupta <prince88aditya@gmail.com>
2021-03-02 22:31:05 +0530

    Directed Graph Decomposition

---
+++ graph_decomposition2/acyclicity.cpp
@@ -0,0 +1,51 @@
+// Check for cycle in a directed graph 
+
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+
+using std::vector;
+using std::pair;
+using std::unordered_set;
+
+void Explore(vector<bool> &Visited, vector<vector<int> > &adj, int vertex, bool &flag, unordered_set <int> &track){
+  Visited[vertex]=true;
+  track.insert(vertex);             // Maintaining a set of vertices visited during a routine, if we visit any vertex twice, presence of cycle is confirmed 
+  vector<int> neighbours=adj[vertex];
+  for(int i=0;i<neighbours.size();i++){
+    if (Visited[neighbours[i]]!=true)
+      Explore(Visited,adj,neighbours[i],flag,track);
+    else if (track.find(neighbours[i])!=track.end())  
+      flag=true;
+  }
+  track.erase(track.find(vertex)); // Remove the vertex after it's exploration is done
+}
+
+int acyclic(vector<bool> &Visited, vector<vector<int> > &adj, int n){
+  bool flag=false;
+  for (int i=0;i<n;i++){
+    if(Visited[i]!=true){
+      unordered_set <int> track;        // Unordered set for O(1) find  
+      Explore(Visited,adj,i,flag,track);
+      if (flag)
+        return 1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  vector<bool> Visited;
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+  }
+  for (size_t i = 0; i < n; i++)
+    Visited.push_back(false);
+
+  std::cout << acyclic(Visited,adj,n);
+}
---
+++ graph_decomposition2/toposort.cpp
@@ -0,0 +1,65 @@
+ // Topological Sort 
+//  Graph should be a DAG
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using std::vector;
+using std::pair;
+
+bool SortBySecond(const pair<int,int> &A, const pair<int,int> &B){ 
+    return (A.second > B.second); 
+}
+
+
+void Explore(vector<bool> &Visited, vector<vector<int> > &adj, int vertex, vector< pair <int,int>> &Map, int &postNumber){
+  Visited[vertex]=true;
+  vector<int> neighbours=adj[vertex];
+  for(int i=0;i<neighbours.size();i++){
+    if (Visited[neighbours[i]]!=true)
+      Explore(Visited,adj,neighbours[i],Map,postNumber);
+  }
+  Map[vertex].second=++postNumber;
+}
+
+void DFS(vector<bool> &Visited, vector<vector<int> > &adj, int n, vector< pair <int,int>> &Map){
+  int postNumber=0;
+  for (int i=0;i<n;i++){
+    if(Visited[i]!=true){
+      Explore(Visited,adj,i,Map,postNumber);
+    }
+  }
+}     
+
+vector<int> toposort(vector<vector<int> > adj) {
+  vector< pair <int,int>> Map;
+  vector<int> order;
+  vector<bool> Visited;
+  for (size_t i = 0; i < adj.size(); i++){
+    Visited.push_back(false);
+    Map.push_back(std::make_pair(i,0));
+  }
+  DFS(Visited,adj,adj.size(),Map);
+  sort(Map.begin(),Map.end(),SortBySecond);                // Just sort vertices in descending order of postNumber  
+  for (int i=0;i<Map.size();i++){
+    //std::cout<<Map[i].first+1<<", "<<Map[i].second<<"\n";
+    order.push_back(Map[i].first);
+  }
+  return order;
+}
+
+int main() {
+  size_t n, m;
+  std::cin >> n >> m;
+  vector<vector<int> > adj(n, vector<int>());
+  for (size_t i = 0; i < m; i++) {
+    int x, y;
+    std::cin >> x >> y;
+    adj[x - 1].push_back(y - 1);
+  }
+  vector<int> order = toposort(adj);
+  for (size_t i = 0; i < order.size(); i++) {
+    std::cout << order[i] + 1 << " ";                      // More than one linear ordering is possible
+  }
+}
